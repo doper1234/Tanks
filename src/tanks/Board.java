e288f101f7695ccc35164863fa843c248a5321ec
Piotr Owsiak <piotr.owsiak@o2.pl>
2015-04-14 22:21:45 +0200

    trying to make it build

--- src/tanks/Board.java
+++ src/tanks/Board.java
@@ -8,16 +8,12 @@ package tanks;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
-import sun.applet.Main;
 
 /**
  *
